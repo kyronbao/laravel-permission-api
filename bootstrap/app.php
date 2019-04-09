e325d93d0357b41633d4098d1c8adf264aebba17
Kyron Bao <kyronbao@gmail.com>
2019-04-10 02:27:06 +0800

    Change error code location 2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,6 @@
 | the IoC container for the system binding all of the various parts.
 |
 */
-
-require_once __DIR__.'/error.php';
-
 $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
