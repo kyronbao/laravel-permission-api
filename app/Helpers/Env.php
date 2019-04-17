3961bb9610b33547f3d3576c347788b69a0c0463
Kyron Bao <kyronbao@gmail.com>
2019-04-18 01:19:24 +0800

    Add Env string for global refer

---
+++ app/Helpers/Env.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kyron Bao
+ * Date: 19-4-18
+ * Time: 上午1:06
+ */
+
+namespace App\Helpers;
+
+
+class Env
+{
+    const PROD = 'production';
+    const TEST = 'test';
+    const LOCAL = 'local';
+}
