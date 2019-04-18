667522de82b73e184bbf4c8a37b33ef27d23a66c
Kyron Bao <kyronbao@gmail.com>
2019-04-18 20:52:46 +0800

    Refactor admin router controller name

--- routes/admin.php
+++ routes/admin.php
@@ -1,16 +1,16 @@
 <?php
 
 
-use Admin\Http\Controllers;
+use Admin\Http\Controllers\PermissionController;
 
 
-Route::post('post-roles', Controllers\PermissionController::class . '@postRoles');
-Route::get('get-roles', Controllers\PermissionController::class . '@getRoles');
-Route::post('post-permissions-via-role', Controllers\PermissionController::class . '@postPermissionsViaRole');
-Route::get('get-permissions-via-role', Controllers\PermissionController::class . '@getPermissionsViaRole');
+Route::post('post-roles', PermissionController::class . '@postRoles');
+Route::get('get-roles', PermissionController::class . '@getRoles');
+Route::post('post-permissions-via-role', PermissionController::class . '@postPermissionsViaRole');
+Route::get('get-permissions-via-role', PermissionController::class . '@getPermissionsViaRole');
 
-Route::post('post-permissions', Controllers\PermissionController::class . '@postPermissions');
-Route::get('get-permissions', Controllers\PermissionController::class . '@getPermissions');
+Route::post('post-permissions', PermissionController::class . '@postPermissions');
+Route::get('get-permissions', PermissionController::class . '@getPermissions');
 
-Route::post('post-roles-via-user', Controllers\PermissionController::class . '@postRolesViaUser');
-Route::get('get-roles-via-user', Controllers\PermissionController::class . '@getRolesViaUser');
+Route::post('post-roles-via-user', PermissionController::class . '@postRolesViaUser');
+Route::get('get-roles-via-user', PermissionController::class . '@getRolesViaUser');
